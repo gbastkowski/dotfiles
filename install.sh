8fe9077e36089a1dc17461da6838be50c64ac062
Gunnar Bastkowski <gunnar@bastkowski.name>
2018-01-16 13:03:14 +0100

    Fix oh-my-zsh installation

--- install.sh
+++ install.sh
@@ -3,3 +3,6 @@
 ln -sf $(pwd)/home/.spacemacs ~
 ln -sf $(pwd)/spacemacs       ~/.emacs.d
 ln -sf $(pwd)/sbt             ~/.sbt
+ln -sf $(pwd)/oh-my-zsh       ~/.oh-my-zsh
+
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k

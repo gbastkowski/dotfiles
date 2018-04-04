81a3286f441b4f37767422734cbab4457ad74a62
Gunnar Bastkowski <gunnar.bastkowski@enfore.com>
2018-04-04 13:21:15 +0200

    Some changes

--- spacemacs-layers/gunnar-ui/config.el
+++ spacemacs-layers/gunnar-ui/config.el
@@ -2,11 +2,8 @@
 (set-frame-height (selected-frame) 70)
 (set-frame-width (selected-frame) 200)
 
-;; Fix MacOS key bindings
-(setq mac-command-key-is-meta t
-      mac-command-modifier 'meta
-      mac-right-command-modifier 'meta
-      mac-option-modifier nil
+;; MacOS key bindings
+(setq mac-option-modifier nil
       ns-function-modifier 'super)
 
 (setq frame-title-format '(buffer-file-name "%f" "Emacs: %b"))

92c0b924bd834dc89c459b28a79cdbe60aaae9bf
Gunnar Bastkowski <gunnar@bastkowski.name>
2018-05-02 23:00:17 +0200

    Remove password=store submodule

--- install.sh
+++ install.sh
@@ -14,3 +14,4 @@ ln -sf $(pwd)/sbt              ~/.sbt
 ln -sf $(pwd)/oh-my-zsh        ~/.oh-my-zsh
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone keybase://private/gbastkowski/password-store ../password-store

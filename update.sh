4a0ab066b66be468f681dbcae66abc8e1fe3fdf7
Gunnar Bastkowski <gunnar.bastkowski@enfore.com>
2018-12-21 10:35:08 +0100

    Update dotfiles and submodules

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+echo "updating dotfiles ..."
+
+echo "updating oh-my-zsh ..."
+git -C oh-my-zsh fetch --all
+git -C oh-my-zsh merge upstream/master
+echo
+
+echo "updating powerlevel9k ..."
+git -C powerlevel9k pull
+echo
+
+echo "updating spacemacs ..."
+git -C spacemacs fetch --all
+git -C spacemacs merge upstream/develop
+echo
+
+echo "pushing submodules to origin ..."
+git -C oh-my-zsh push
+git -C powerlevel9k push
+git -C spacemacs push
+echo
+echo "pushing dotfiles to origin ..."
+git add .
+git commit -m "Update dotfiles and submodules"
+git push

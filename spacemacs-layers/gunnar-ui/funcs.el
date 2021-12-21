704921272a209da2378116e608d33536c343b44d
Gunnar Bastkowski <gunnar@bastkowski.name>
2021-12-21 14:07:17 +0100

    Add aj-toggle-fold

--- spacemacs-layers/gunnar-ui/funcs.el
+++ spacemacs-layers/gunnar-ui/funcs.el
@@ -18,6 +18,20 @@
   (progn
     (message "Columns: " columns " Lines: " lines)))
 
+; Found at https://stackoverflow.com/questions/1587972/how-to-display-indentation-guides-in-emacs/4459159#4459159
+; and decided that I like it
+(defun aj-toggle-fold ()
+  "Toggle fold all lines larger than indentation on current line"
+  (interactive)
+  (let ((col 1))
+    (save-excursion
+      (back-to-indentation)
+      (setq col (+ 1 (current-column)))
+      (set-selective-display
+       (if selective-display nil (or col 1))))))
+
+(spacemacs/set-leader-keys "of" 'aj-toggle-fold)
+
 
 ;; (set-frame-height (selected-frame) 50)
 ;; (set-frame-width (selected-frame) 140)

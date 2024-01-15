bd95ad66c451a23718def31026800e07fd395f9b
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-01-15 18:34:43 +0100

    Put mail config in layer

--- emacs/.spacemacs-layers/gunnar-mu4e/packages.el
+++ emacs/.spacemacs-layers/gunnar-mu4e/packages.el
@@ -1,24 +1,46 @@
 (defconst gunnar-mu4e-packages '(mu4e))
 
 (defun gunnar-mu4e/post-init-mu4e ()
-  (setq mu4e-maildir "~/.mail/gmail"
-        user-full-name "Gunnar Bastkowski"
-        user-mail-address "gunnar.bastkowski@enfore.com"
-        mu4e-inbox-folder "/Inbox"
-        mu4e-drafts-folder "/Drafts"
-        mu4e-sent-folder "/Sent"
-        mu4e-trash-folder "/Trash"
-        mu4e-refile-folder "/Archive"
-        mu4e-get-mail-command "mbsync gmail && mu index -m ~/.mail/gmail"
-        mu4e-update-interval 120 ; seconds
-        mu4e-compose-signature-auto-include nil
-        mu4e-view-show-images t
-        mu4e-view-show-addresses t
+
+  (setq mail-user-agent 'mu4e-user-agent
+
+        mu4e-drafts-folder "/[Gmail].Drafts"
+        mu4e-sent-folder   "/[Gmail].Sent Mail"
+        mu4e-trash-folder  "/[Gmail].Trash"
+        ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
+        mu4e-sent-messages-behavior 'delete
+        mu4e-maildir-shortcuts '( (:maildir "/INBOX"              :key ?i)
+                                  (:maildir "/[Gmail].Sent Mail"  :key ?s)
+                                  (:maildir "/[Gmail].Trash"      :key ?;TODO: )
+                                  (:maildir "/[Gmail].All Mail"   :key ?a)))
+
+        ;; allow for updating mail using 'U' in the main view:
+        mu4e-get-mail-command "offlineimap"
+
+        user-mail-address "gunnar.bastkowski@gmail.com"
+        user-full-name  "Gunnar Bastkowski"
+        mu4e-compose-signature (concat "Gunnar Bastkowski")
+
+        starttls-use-gnutls t
+
+        smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+        smtpmail-auth-credentials '(("smtp.gmail.com" 587 "gunnar.bastkowski@gmail.com" nil))
+        smtpmail-default-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-server "smtp.gmail.com"
+        smtpmail-smtp-service 587
+
+        message-send-mail-function 'smtpmail-send-it
+        message-kill-buffer-on-exit t
+        ;; Stop creating backups and lock files
+        create-lockfiles nil
+        backup-directory-alist '((".*" . "~/.Trash"))
+
+        mu4e-compose-signature-auto-include t
         mu4e-enable-notifications t
         mu4e-enable-mode-line t
-        ;; don't save message to Sent Messages, GMail/IMAP will take care of this
-        mu4e-sent-messages-behavior 'delete
-        mu4e-maildir-shortcuts '(("/Inbox" . ?i)))
+        mu4e-update-interval 120 ; seconds
+        )
 
-  (with-eval-after-load 'mu4e-alert (mu4e-alert-set-default-style 'notifier))
-  )
+  (add-to-list 'mu4e-bookmarks
+               ;; ':favorite t' i.e, use this one for the modeline
+               '(:query "maildir:/inbox" :name "Inbox" :key ?i :favorite t)))

cae5de59e048a9c408d8dcae02cdc601d4b1c799
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-02-09 00:29:45 +0100

    Adjust update.sh to new directories

--- update.sh
+++ update.sh
@@ -34,14 +34,28 @@ echo "updating zsh-vi-mode ..."
 git -C zsh-vi-mode pull
 echo
 
-echo "updating spacemacs ..."
+echo "updating chemacs2 ..."
 git -C emacs/.emacs.d fetch --all
-EDITOR=vim git -C emacs/.emacs.d checkout develop
-EDITOR=vim git -C emacs/.emacs.d merge upstream/develop
+EDITOR=vim git -C emacs/.emacs.d checkout main
+EDITOR=vim git -C emacs/.emacs.d merge upstream/main
 EDITOR=vim git -C emacs/.emacs.d push
-EDITOR=vim git -C emacs/.emacs.d checkout gunnar
-EDITOR=vim git -C emacs/.emacs.d merge develop
-EDITOR=vim git -C emacs/.emacs.d push
+echo
+
+echo "updating spacemacs ..."
+git -C emacs/.emacs.spacemacs fetch --all
+EDITOR=vim git -C emacs/.emacs.spacemacs checkout develop
+EDITOR=vim git -C emacs/.emacs.spacemacs merge upstream/develop
+EDITOR=vim git -C emacs/.emacs.spacemacs push
+EDITOR=vim git -C emacs/.emacs.spacemacs checkout gunnar
+EDITOR=vim git -C emacs/.emacs.spacemacs merge develop
+EDITOR=vim git -C emacs/.emacs.spacemacs push
+echo
+
+echo "updating doomemacs ..."
+git -C emacs/.emacs.doom fetch --all
+EDITOR=vim git -C emacs/.emacs.doom checkout master
+EDITOR=vim git -C emacs/.emacs.doom merge upstream/master
+EDITOR=vim git -C emacs/.emacs.doom push
 echo
 
 echo "updating reveal.js ..."

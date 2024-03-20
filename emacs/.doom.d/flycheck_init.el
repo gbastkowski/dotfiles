f9cf207dd7fdffa8495396fa9e84ebec8c992a7e
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-03-20 22:39:28 +0100

    Upgrade emacs stuff

---
+++ emacs/.doom.d/flycheck_init.el
@@ -0,0 +1,25 @@
+;;; flycheck_init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 Gunnar Bastkowski
+;;
+;; Author: Gunnar Bastkowski <gunnar@bastkowski.name>
+;; Maintainer: Gunnar Bastkowski <gunnar@bastkowski.name>
+;; Created: March 20, 2024
+;; Modified: March 20, 2024
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/gbastkowski/flycheck_init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'flycheck_init)
+;;; flycheck_init.el ends here

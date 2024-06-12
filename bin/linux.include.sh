1025b055fd24a1067a15e135d8f979a4776a5c58
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-06-12 23:23:04 +0200

    Update dotfiles and submodules

---
+++ bin/linux.include.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+upgrade_system_and_packages() {
+    yay -Syu
+}
---
+++ bin/macos.include.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+upgrade_system_and_packages() {
+    softwareupdate -l
+    brew update && brew upgrade
+}
+
+upgrade_pip() {
+    echo "updating pip stuff"
+    for i in $(pip list -o | awk 'NR > 2 {print $1}')
+    do
+        pip install -U $i
+    done
+}

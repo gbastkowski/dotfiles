0380bdbbc38abb34d3048e85dcbae008ed861d03
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-07-03 00:14:22 +0200

    Add eww config

---
+++ eww/listen_hyprland_events.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+handle_hyprland_event() {
+    case $1 in
+        workspacev2*) echo "${1#*>>}" | awk -F ',' '{print $1}' ;;
+    esac
+}
+
+listen_hyprland_events() {
+    SOCKET=UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock
+    socat -U - $SOCKET | while read -r line; do handle_hyprland_event "$line"; done
+}
+
+listen_hyprland_events

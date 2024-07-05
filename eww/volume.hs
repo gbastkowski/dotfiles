023313fef830a3b49d6ed08a5e6bf65c63440a49
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-07-06 01:00:28 +0200

    Add some Haskell stuff for the moment

--- eww/volume.hs
+++ eww/volume.hs
@@ -1,10 +1,22 @@
 #!/usr/bin/env stack
-{- stack script --resolver lts-17.9
+{- stack script --resolver lts-20.26
   --optimize
+  --package alsa-core
   --package alsa-mixer
   --package process
 -}
 
-main :: IO ()
+import Sound.ALSA.Mixer
 
-main = putStrLn "Hello, World"
+main :: IO ()
+main = do
+  putStrLn "Hello, World"
+  toggleMute
+
+toggleMute :: IO ()
+toggleMute =
+  withMixer "default" $ \mixer ->
+    do Just control <- getControlByName mixer "Master"
+       let Just playbackSwitch = playback $ switch control
+       Just sw <- getChannel FrontLeft playbackSwitch
+       setChannel FrontLeft playbackSwitch $ not sw

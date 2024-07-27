281082d595410b1d62c668ad9a78911685631c24
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-07-28 00:37:23 +0200

    Fix elisp error

--- eww/modules/emacs/emacs.sh
+++ eww/modules/emacs/emacs.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/bash
 
 function current_task() {
-    result=$(emacsclient -e '(substring-no-properties org-clock-current-task)')
-    if [ -n "$result" ]; then
-        icon="\"󰔛\""
-    else
+    result=$(emacsclient -e '(if org-clock-current-task (substring-no-properties org-clock-current-task) "")')
+    if [[ "$result" == "\"\"" ]]; then
         icon="\"\""
+    else
+        icon="\"󰔛\""
     fi
     echo "{ \"text\" : $result, \"icon\" : $icon}"
 }
 
-if   [[ "$1" == "--current-task" ]]; then current_task
+if [[ "$1" == "--current-task" ]]; then current_task
 fi

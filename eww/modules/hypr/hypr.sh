b0e4f78b7b8d699c3d9df06889cf792fb15c3cd8
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-07-28 02:25:27 +0200

    Implement event for hyprctl activewindow

--- eww/modules/hypr/hypr.sh
+++ eww/modules/hypr/hypr.sh
@@ -1,8 +1,22 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
-function active_window () {
-    hyprctl -j activewindow | jq -r '.title'
+active_window () {
+    hyprctl -j activewindow | jq -c '.'
 }
 
-if   [[ "$1" == "--active-window" ]]; then active_window
+active_workspace () {
+    hyprctl -j activeworkspace | jq -c '.'
+}
+
+build_json () {
+    echo "{ \"activewindow\" : $(active_window), \"activeworkspace\" : $(active_workspace) }"
+}
+
+listen() {
+    SOCKET=UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock
+    socat -U - $SOCKET | while read -r line; do build_json $line; done
+}
+
+if [[ "$1" == "--listen" ]]; then listen "$2"
+else build_json
 fi

5dd029a23023b67a14c597871159d15411d000ff
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-08-31 11:59:50 +0200

    Update dotfiles and submodules

--- emacs/.doom.d/custom.el
+++ emacs/.doom.d/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("/home/gunnar/org/inbox-akiko.org" "/home/gunnar/org/inbox-galaxy.org" "/home/gunnar/org/inbox-s7.org" "/home/gunnar/org/inbox-yesomeo.org" "/home/gunnar/org/tickler.org" "/home/gunnar/org/projects.org" "/home/gunnar/org/projects.org_archive" "/home/gunnar/org/journal/2024/07/26.org")))
+   '("/home/gunnar/org/inbox-akiko.org" "/home/gunnar/org/inbox-galaxy.org" "/home/gunnar/org/inbox-s7.org" "/home/gunnar/org/inbox-yesomeo.org" "/home/gunnar/org/tickler.org" "/home/gunnar/org/projects.org" "/home/gunnar/org/projects.org_archive" "/home/gunnar/org/journal/2024/08/31.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

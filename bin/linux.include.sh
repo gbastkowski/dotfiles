efe5ed4cd1e8aaaa1c7e906cf079bc60efa00cd5
Gunnar Bastkowski <gunnar@bastkowski.name>
2024-11-27 15:17:38 +0100

    Fix hpyrland plugins

--- bin/linux.include.sh
+++ bin/linux.include.sh
@@ -5,6 +5,7 @@ upgrade_system_and_packages() {
     git -C ~/git/hyprwm/Hyprland pull
     make -C ~/git/hyprwm/Hyprland all
     sudo make -C ~/git/hyprwm/Hyprland install
+    hyprpm update
 }
 
 upgrade_python_packages() {

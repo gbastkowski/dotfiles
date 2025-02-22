89798407d13ccb3f310b742fb46e8445b2435f99
Gunnar Bastkowski <gunnar@bastkowski.name>
2025-02-22 15:34:36 +0100

    Update openai playground

--- emacs/.doom.d/playground/openai/openai-playground.el
+++ emacs/.doom.d/playground/openai/openai-playground.el
@@ -7,9 +7,9 @@
 ;; Created: February 20, 2025
 ;; Modified: February 20, 2025
 ;; Version: 0.0.1
-;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
-;; Homepage: https://github.com/gunnar/openai-playground
-;; Package-Requires: ((emacs "24.3"))
+;; Keywords:  hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/gbastkowski/openai-playground
+;; Package-Requires: ((emacs "25.1"))
 ;;
 ;; This file is not part of GNU Emacs.
 ;;
@@ -55,27 +55,57 @@
               (lambda (&rest args &key error-thrown &allow-other-keys)
                 (message "Failed to align code: %S" error-thrown))))))
 
-(request "https://api.openai.com/v1/chat/completions"
-  :type "POST"
-  :headers `(("Content-Type" . "application/json")
-             ("Authorization" . ,(concat "Bearer " (password-store-get "private/openai/emacs-api-key"))))
-  :data (json-encode `(
-                       ("model" . ,"gpt-4o-mini")
-                       ("messages" . ((("role" . "user")
-                                       ("content" . ,(concat "Say hello\n\n" "Gunnar")))))))
-  ;; :parser 'buffer-string
+(setq openai-completion-api "https://api.openai.com/v1/chat/completions")
+(setq openai-completion-model "gpt-4o-mini")
+
+(setq openai-headers `(("Content-Type" . "application/json")
+                       ("Authorization" . ,(concat "Bearer " (password-store-get "private/openai/emacs-api-key")))))
+
+(defun openai-completion-create-request-body (messages &optional log)
+  (let ((body (json-encode `(("model"    . ,openai-completion-model)
+                             ("messages" . ,(mapcar (lambda (pair)
+                                                      `(("role"    . ,(symbol-name (car pair)))
+                                                        ("content" . ,(cdr pair))))
+                                                    messages))))))
+    (when log
+      (with-current-buffer (get-buffer-create "*openai completion log*")
+        (goto-char (point-max))
+        (insert body)
+        (insert "\n")))
+    body))
+
+(defun openai-completion-response-extract-messages (data)
+  (let* ((choices (alist-get 'choices data))
+        (first-choice (aref choices 0))
+        (message (alist-get 'message first-choice))
+        (content (alist-get 'content message)))
+    (format "%s\n" content)))
+
+(defun openai-completion-log-to-buffer (data &optional pop)
+  (with-current-buffer (get-buffer-create "*openai completion log*")
+    (goto-char (point-max))
+    (insert (openai-completion-response-extract-messages data))
+    (when pop (pop-to-buffer (current-buffer)))))
+
+(setq openai-completion-messages
+      '((assistant . "I am very brief and don't answer additional text.")
+        (user      . "Say hello to Gunnar")))
+
+(defun openai-completion-messages-explain-code (language code)
+  `((assistant . "I am very brief")
+    (user      . ,(format "Please explain the following code:\n%s" code))))
+
+(request openai-completion-api :type "POST" :headers openai-headers
+  :data (openai-completion-create-request-body openai-completion-messages)
   :parser 'json-read
-  :success
-  (cl-function (lambda (&key data &allow-other-keys)
-                 (when data
-                   (with-current-buffer (get-buffer-create "*request demo*")
-                     (erase-buffer)
-                     (insert (format "%s"
-                                     (assoc 'message (aref (cdr (assoc 'choices data)) 0))))
-                     (pop-to-buffer (current-buffer))))))
-  :error
-  (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
-                 (message "Got error: %S" error-thrown)))
+  :success (cl-function (lambda
+                          (&key data &allow-other-keys)
+                          (when data (openai-completion-log-to-buffer data))))
+
+  :error (cl-function (lambda (&rest args &key error-thrown &allow-other-keys)
+                        (message "Got error: %S" error-thrown)))
+
   :complete (lambda (&rest _) (message "Finished!"))
+
   :status-code '((400 . (lambda (&rest _) (message "Got 400.")))
                  (418 . (lambda (&rest _) (message "Got 418.")))))

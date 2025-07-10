efa1bd8c7c2084877ad598a8e99d86006d3a4ca5
Gunnar Bastkowski <gunnar@bastkowski.name>
2025-07-10 15:11:34 +0200

    Enable direnv support in Emacs

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -68,7 +68,7 @@
                     ;;biblio                          ; Writes a PhD for you (citation needed)
                     ;;collab                          ; buffers with friends
                     ;;debugger                        ; FIXME stepping through code, to help you add bugs
-                    ;;direnv
+                    direnv
                     docker
                     editorconfig
                     ;;ein                             ; tame Jupyter notebooks with emacs

2978466505440f6b9856187254a2dfda5a9a09da
Gunnar Bastkowski <gunnar@bastkowski.name>
2025-12-15 22:38:08 +0100

    Adjust emacs config a little

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -131,7 +131,7 @@
                     ;;nim                             ; python + lisp at the speed of c
                     ;;nix                             ; I hereby declare "nix geht mehr!"
                     ;;ocaml                           ; an objective camel
-                    (org +brain +crypt +dragndrop +journal +pandoc +passwords +pomodoro +present +pretty +roam2)
+                    (org +brain +crypt +dragndrop +journal +pandoc +passwords +pomodoro +present +pretty +roam)
                     ;;php                             ; perl's insecure younger brother
                     plantuml
                     ;;purescript                      ; javascript, but functional

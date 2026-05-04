0a520fdfacd3f525714fba09f33483ffc37d55b4
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-04 12:12:31 +0200

    Migrate zsh config to Home Manager

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -5,6 +5,186 @@
   home.stateVersion = "25.11";
 
   home.file.".p10k.zsh".source = ../../zsh/.p10k.zsh;
+
+  programs.zsh = {
+    enable = true;
+
+    autosuggestion.enable = false;
+    syntaxHighlighting.enable = false;
+
+    oh-my-zsh = {
+      enable = true;
+      theme = "powerlevel10k/powerlevel10k";
+      plugins = [
+        "common-aliases"
+        "direnv"
+        "docker"
+        "docker-compose"
+        "dotenv"
+        "extract"
+        "fasd"
+        "gem"
+        "git"
+        "git-lfs"
+        "gitignore"
+        "gpg-agent"
+        "helm"
+        "history"
+        "history-substring-search"
+        "kubectl"
+        "mvn"
+        "nvm"
+        "pass"
+        "sbt"
+        "scala"
+        "sdk"
+        "screen"
+        "virtualenv"
+        "zsh-vi-mode"
+      ];
+      extraConfig = ''
+        ZSH_DOTENV_FILE=.env.local
+        ZSH_DISABLE_COMPFIX=true
+        DISABLE_AUTO_UPDATE="true"
+        COMPLETION_WAITING_DOTS="true"
+      '';
+    };
+
+    sessionVariables = {
+      TERM = "xterm-256color";
+      DEFAULT_USER = "gunnar";
+      GTAGSLABEL = "pygments";
+      EDITOR = "emacsclient -t";
+      OPENCODE_EXPERIMENTAL = "true";
+      NODE_REPL_HISTORY = "~/.node_history";
+      NODE_REPL_HISTORY_SIZE = "32768";
+      NODE_REPL_MODE = "sloppy";
+      PYTHONIOENCODING = "UTF-8";
+      HISTSIZE = "32768";
+      HISTFILESIZE = "32768";
+      HISTCONTROL = "ignoreboth";
+      LANG = "en_US.UTF-8";
+      LC_ALL = "en_US.UTF-8";
+      MANPAGER = "less -X";
+      SDKMAN_DIR = "$HOME/.sdkman";
+      NVM_DIR = "$HOME/.nvm";
+    };
+
+    initContent = ''
+      # Return early for IntelliJ environment reader
+      if [[ -n "$INTELLIJ_ENVIRONMENT_READER" ]]; then
+        return
+      fi
+
+      # p10k instant prompt
+      if [[ -r "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh" ]]; then
+        source "''${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-''${(%):-%n}.zsh"
+      fi
+
+      # PATH helpers
+      _path_contains() {
+        dir=$1
+        [[ -z ''${dir:-} ]] && return 1
+        case ":''${PATH:-}:" in *:"$dir":*) return 0 ;; esac
+        return 1
+      }
+      path_prepend() {
+        dir=$1
+        [[ -z ''${dir:-} ]] && return
+        if ! _path_contains "$dir"; then
+          PATH="$dir''${PATH:+:$PATH}"
+          export PATH
+        fi
+      }
+      path_append() {
+        dir=$1
+        [[ -z ''${dir:-} ]] && return
+        if ! _path_contains "$dir"; then
+          PATH="''${PATH:+$PATH:}$dir"
+          export PATH
+        fi
+      }
+
+      # JAVA_HOME
+      ensure_java_home() {
+        if [[ -n ''${SDKMAN_DIR:-} && -d ''${SDKMAN_DIR}/candidates/java/current ]]; then
+          export JAVA_HOME="''${SDKMAN_DIR}/candidates/java/current"
+        elif command -v /usr/libexec/java_home >/dev/null 2>&1; then
+          local j
+          j=$(/usr/libexec/java_home 2>/dev/null || true)
+          [[ -n $j ]] && export JAVA_HOME="$j"
+        else
+          local jp
+          jp="$(command -v java 2>/dev/null || true)"
+          [[ -n $jp && $jp == */bin/java ]] && export JAVA_HOME="''${jp%/bin/java}"
+        fi
+      }
+      ensure_java_home
+
+      # GPG
+      if command -v gpg-connect-agent >/dev/null 2>&1; then
+        export GPG_TTY="$(tty)"
+        gpg-connect-agent updatestartuptty /bye >/dev/null 2>&1 || true
+      fi
+
+      # kitty socket
+      if [[ -n "''${KITTY_WINDOW_ID:-}" ]]; then
+        export KITTY_LISTEN_ON="''${KITTY_LISTEN_ON:-unix:/tmp/kitty-$USER}"
+      fi
+
+      # Private/work config
+      [[ -f $HOME/.ista_rc ]] && source "$HOME/.ista_rc"
+      [[ -f $HOME/.private ]] && source "$HOME/.private"
+
+      # p10k config
+      [[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+
+      # PATH additions
+      [[ -d $HOME/.bin ]] && path_prepend "$HOME/.bin"
+      [[ -d $HOME/go/bin ]] && path_prepend "$HOME/go/bin"
+      path_prepend "./node_modules/.bin"
+      [[ -d $HOME/.emacs.doom/bin ]] && path_append "$HOME/.emacs.doom/bin"
+      [[ -d $HOME/.rbenv/bin ]] && path_prepend "$HOME/.rbenv/bin"
+      [[ -d $HOME/.local/bin ]] && path_append "$HOME/.local/bin"
+
+      # rbenv
+      eval "$(rbenv init - zsh)"
+
+      # sdkman
+      [[ -s "$SDKMAN_DIR/bin/sdkman-init.sh" ]] && source "$SDKMAN_DIR/bin/sdkman-init.sh"
+
+      # nvm
+      if [[ "$(uname)" == "Linux" ]]; then
+        [[ -s "/usr/share/nvm/init-nvm.sh" ]] && source "/usr/share/nvm/init-nvm.sh"
+      fi
+
+      # Dart completions (Linux only)
+      [[ -f /home/gunnar/.dart-cli-completion/zsh-config.zsh ]] && source /home/gunnar/.dart-cli-completion/zsh-config.zsh || true
+
+      # Arch-specific
+      case "$(uname -a)" in
+        *arch*)
+          [[ -n "$SSH_CONNECTION" ]] && systemctl --user start inhibit-suspend.service
+          ;;
+      esac
+
+      # local bin helper
+      [[ -f "$HOME/.local/bin/ensure-local-bin-in-path" ]] && source "$HOME/.local/bin/ensure-local-bin-in-path"
+
+      # Shell functions
+      mcd() { mkdir "$1" && cd "$1"; }
+      pman() {
+        for name in "$@"; do
+          man -t "$name" | pstopdf -i
+          mv -f stdin.pdf ~/Documents/man-pages/"$name".pdf
+          open ~/Documents/man-pages/"$name".pdf
+        done
+      }
+
+      unalias run-help 2>/dev/null || true
+      autoload run-help
+    '';
+  };
   home.file.".oh-my-zsh/custom/themes/powerlevel10k".source = inputs.powerlevel10k;
   home.file.".oh-my-zsh/custom/plugins/zsh-vi-mode".source = inputs.zsh-vi-mode;
   home.file.".sbt/1.0/build.sbt".source = ../../sbt/.sbt/1.0/build.sbt;

2ee896a76a8cf54484c6ef732792adc91923f2f8
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-05 10:57:52 +0200

    Migrate bin scripts to Home Manager; restore kitty config files

--- nix/home/common.nix
+++ nix/home/common.nix
@@ -5,6 +5,22 @@
   home.stateVersion = "25.11";
 
   home.file.".p10k.zsh".source = ../../zsh/.p10k.zsh;
+  home.file.".local/bin/checkmail.sh"        = { source = ../../bin/checkmail.sh;        executable = true; };
+  home.file.".local/bin/emacs-capture.sh"    = { source = ../../bin/emacs-capture.sh;    executable = true; };
+  home.file.".local/bin/emacs-upgrade.sh"    = { source = ../../bin/emacs-upgrade.sh;    executable = true; };
+  home.file.".local/bin/eos-capture-video.sh"= { source = ../../bin/eos-capture-video.sh;executable = true; };
+  home.file.".local/bin/git-ensure-remotes.sh"={ source = ../../bin/git-ensure-remotes.sh;executable = true; };
+  home.file.".local/bin/install-xmlls.sh"    = { source = ../../bin/install-xmlls.sh;    executable = true; };
+  home.file.".local/bin/linux.include.sh"    = { source = ../../bin/linux.include.sh;    executable = true; };
+  home.file.".local/bin/macos.include.sh"    = { source = ../../bin/macos.include.sh;    executable = true; };
+  home.file.".local/bin/metals-emacs"        = { source = ../../bin/metals-emacs;        executable = true; };
+  home.file.".local/bin/ollama-coder.sh"     = { source = ../../bin/ollama-coder.sh;     executable = true; };
+  home.file.".local/bin/ollama-install.sh"   = { source = ../../bin/ollama-install.sh;   executable = true; };
+  home.file.".local/bin/ostype.sh"           = { source = ../../bin/ostype.sh;           executable = true; };
+  home.file.".local/bin/release.sh"          = { source = ../../bin/release.sh;          executable = true; };
+  home.file.".local/bin/system-upgrade.sh"   = { source = ../../bin/system-upgrade.sh;   executable = true; };
+  home.file.".local/bin/termux.include.sh"   = { source = ../../bin/termux.include.sh;   executable = true; };
+
   home.file.".android-env".source = ../../home/.android-env;
   home.file.".ideavimrc".source = ../../idea/.ideavimrc;
   home.file.".uniteai.yml".source = ../../home/.uniteai.yml;

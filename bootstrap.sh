a40226d6257cfec327954ef44c01a1d450473b2b
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-07 00:21:33 +0200

    Merge bootstrap scripts into single bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+repo_root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+if_confirmed() {
+	read -r -p "$1 [Y/n] " answer || true
+	case "${answer:-Y}" in
+        [Yy] | [Yy][Ee][Ss] | "") return 0 ;;
+        *)                        return 1 ;;
+	esac
+}
+
+case "$(uname -s)" in
+  Darwin)
+    hm_target="darwin-dotfiles"
+    if_confirmed "Do you want to install nix?"                                &&  sh <(curl -L https://nixos.org/nix/install)
+    ;;
+  Linux)
+    hm_target="arch-dotfiles"
+    if_confirmed "Do you want to install nix?"                                &&  yay -S nix
+    if_confirmed "Do you want to enable the nix daemon service?"              &&  sudo systemctl enable --now nix-daemon.service
+    ;;
+  *)
+    echo "Unsupported OS: $(uname -s)"
+    exit 1
+    ;;
+esac
+
+if_confirmed "Do you want to link nix/config to ~/.config/nix?"           &&  ln -sfn "$repo_root/nix/config" "$HOME/.config/nix"
+if_confirmed "Do you want to verify flakes with 'nix flake show ./nix'?"  &&  cd "$repo_root" && nix flake show ./nix
+if_confirmed "Do you want to build the Home Manager config?"              &&  cd "$repo_root" && nix run github:nix-community/home-manager/release-25.11 -- build --flake "./nix#$hm_target"
+if_confirmed "Do you want to switch to the Home Manager config?"          &&  cd "$repo_root" && nix run github:nix-community/home-manager/release-25.11 -- switch -b backup --flake "./nix#$hm_target"
+if_confirmed "Do you want to install Chemacs2 (Emacs profile switcher)?"  &&  git clone https://github.com/plexus/chemacs2.git /tmp/chemacs2 && cp /tmp/chemacs2/{early-init.el,init.el,chemacs.el} "$HOME/.emacs.d/"
+if_confirmed "Do you want to install Doom Emacs?"                         &&  git clone git@github.com:gbastkowski/doomemacs.git "$HOME/.emacs.doom" && "$HOME/.emacs.doom/bin/doom" install

594c5d9c7ed39fda7356f7cd7999e403c3be8f56
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-06 10:57:07 +0200

    Enable nix Doom module and update .emacs.doom submodule

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -152,7 +152,7 @@
                     lua                               ; one-based indices? one-based indices
                     markdown                          ; writing docs for people to ignore
                     ;;nim                             ; python + lisp at the speed of c
-                    ;;nix                             ; I hereby declare "nix geht mehr!"
+                    nix
                     ;;ocaml                           ; an objective camel
                     (org +brain +crypt +dragndrop +journal +pandoc +passwords +pomodoro +present +pretty +roam)
                     ;;php                             ; perl's insecure younger brother

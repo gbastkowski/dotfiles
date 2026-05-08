8dce6b80421c739a90137a466a3f3c0ed70beeb2
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-08 23:24:14 +0200

    Replace gpg-agent.conf with services.gpg-agent, remove gnupg/ source files

--- home/gpg.nix
+++ home/gpg.nix
@@ -1,8 +1,12 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  home.file.".gnupg/gpg-agent.conf".source = ../gnupg/gpg-agent.conf;
-  home.file.".gnupg/pinentry-wrapper" = {
-    source = ../gnupg/pinentry-wrapper;
-    executable = true;
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    defaultCacheTtl = 86400;
+    maxCacheTtl = 86400;
+    defaultCacheTtlSsh = 86400;
+    maxCacheTtlSsh = 86400;
+    pinentry.package = if pkgs.stdenv.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-curses;
   };
 }

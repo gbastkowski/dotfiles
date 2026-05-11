3309dd9273a9fcfed55b531d721f28455623aad7
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-11 13:09:48 +0200

    Expand byobu.nix to link individual files instead of directory

--- home/byobu.nix
+++ home/byobu.nix
@@ -1,4 +1,19 @@
 { ... }:
 {
-  home.file.".byobu".source = ../byobu;
+  home.file = {
+    ".byobu/backend".source      = ../byobu/backend;
+    ".byobu/color".source        = ../byobu/color;
+    ".byobu/color.tmux".source   = ../byobu/color.tmux;
+    ".byobu/datetime.tmux".source = ../byobu/datetime.tmux;
+    ".byobu/keybindings".source  = ../byobu/keybindings;
+    ".byobu/keybindings.tmux".source = ../byobu/keybindings.tmux;
+    ".byobu/profile".source      = ../byobu/profile;
+    ".byobu/profile.tmux".source = ../byobu/profile.tmux;
+    ".byobu/prompt".source       = ../byobu/prompt;
+    ".byobu/status".source       = ../byobu/status;
+    ".byobu/status-e".source     = ../byobu/status-e;
+    ".byobu/statusrc".source     = ../byobu/statusrc;
+    ".byobu/windows".source      = ../byobu/windows;
+    ".byobu/windows.tmux".source = ../byobu/windows.tmux;
+  };
 }

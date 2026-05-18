1444a1d16250d9e62d34e3bb2458a70bbe5d6e78
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-18 16:32:48 +0200

    Collapse emacs restart into single line

--- bin/emacs-upgrade.sh
+++ bin/emacs-upgrade.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 doom upgrade && doom sync --rebuild
 
-systemctl --user restart emacs     && \
-	echo emacs restarted       || \
-	echo emacs restart failed
-
+systemctl --user restart emacs && echo emacs restarted || echo emacs restart failed

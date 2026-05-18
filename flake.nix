8b638e78919cfc3936abb9c9a1bb11eae739aa76
Gunnar Bastkowski <gunnar@bastkowski.name>
2026-05-18 19:41:14 +0200

    Extract mvn/default.nix from ista.nix

--- flake.nix
+++ flake.nix
@@ -51,7 +51,7 @@
       };
     in {
       homeConfigurations = {
-        "ista-dotfiles"  = mkHost "aarch64-darwin" [ ./hammerspoon ./ista.nix ];
+        "ista-dotfiles"  = mkHost "aarch64-darwin" [ ./hammerspoon ./mvn ./ista.nix ];
         "akiko-dotfiles" = mkHost "x86_64-linux"   [ ./hypr ./akiko.nix ];
       };
     };
